701a6213ed0396014a9e8d70fda74020dba4817c
knight <wanderknight@gmail.com>
2020-02-27 13:05:12 +0800

    1.修改为广度优先爬取。以防止深度太大。

--- doubanSpider/doubanSpider/settings.py
+++ doubanSpider/doubanSpider/settings.py
@@ -27,6 +27,10 @@ CLOSESPIDER_ERRORCOUNT = 20
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
+DEPTH_PRIORITY = 1
+SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
